11b840df85e47302ffc6b74916e8c4c4793e4d12
vahermihkel <vahermihkel@gmail.com>
2021-09-14 13:00:15 +0300

    muutujad, tüübid, listid, objektid, objekti-listid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
 import { HomeComponent } from './home/home.component';
 
+// router-outlet asemele tuleb parempoolne component, vastavalt sellele mis on path (url)
+// localhost:4200/ järel
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "ostukorv", component: CartComponent },
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  // MUUTUJA -- string interpolation {{ }}
+  // LISTI -- tsükli abil HTML-s (Loops)
+  // OBJEKTI -- võtme kaudu (mis ma ise võtme nimeks panen) 
+  // LISTI OBJEKTIDEST -- kuvan tsükli abil, mille sees kasutan objekti võtit
+
+  // võimalikud väärtuse tüübid:
+  // string ehk sõnaline ---- "midagi" jutumärkides
+  // number ehk numbriline ---- 0   numbrina
+  // boolean ehk kahendväärtus ---- true   true või false
+  // array ehk massiiv ---- ["","",""] kandiliste sulgude vahel komaga eraldatud elemendid
+  // object ehk objekt ---- {võti: "väärtus", võti2: "väärtus2"} loogeliste sulgude vahel võti-väärtus paarid
+  title = "Ese1";
+  title2 = "Ese2"
+  price = 10;
+  price2 = 20;
+  isActive = true;
+  isActive2 = false;
+
+  titles = ["Ese1", "Ese2"];
+  prices = [10, 20];
+  actives = [true, false];
+
+  item = { title: "Ese1", price: 10, isActive: true };
+  item2 = { title: "Ese2", price: 20, isActive: false };
+
+  itemList = [{ title: "Ese1", price: 10, isActive: true }, { title: "Ese2", price: 20, isActive: false }];
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log("jõudsin cart componenti");
   }
 
 }
