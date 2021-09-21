2e0f945a6e4da4bfe78113ee34b993391ab3172b
vahermihkel <vahermihkel@gmail.com>
2021-09-21 13:07:48 +0300

    ostukorvis: splice, tühjendus, home-s push, componentide suhtlemiseks service

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,37 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  // MUUTUJA -- string interpolation {{ }}
-  // LISTI -- tsükli abil HTML-s (Loops)
-  // OBJEKTI -- võtme kaudu (mis ma ise võtme nimeks panen) 
-  // LISTI OBJEKTIDEST -- kuvan tsükli abil, mille sees kasutan objekti võtit
+  cartItems: any[] = [];
 
-  // võimalikud väärtuse tüübid:
-  // string ehk sõnaline ---- "midagi" jutumärkides
-  // number ehk numbriline ---- 0   numbrina
-  // boolean ehk kahendväärtus ---- true   true või false
-  // array ehk massiiv ---- ["","",""] kandiliste sulgude vahel komaga eraldatud elemendid
-  // object ehk objekt ---- {võti: "väärtus", võti2: "väärtus2"} loogeliste sulgude vahel võti-väärtus paarid
-  title = "Ese1";
-  title2 = "Ese2"
-  price = 10;
-  price2 = 20;
-  isActive = true;
-  isActive2 = false;
-
-  titles = ["Ese1", "Ese2"];
-  prices = [10, 20];
-  actives = [true, false];
-
-  item = { title: "Ese1", price: 10, isActive: true };
-  item2 = { title: "Ese2", price: 20, isActive: false };
-
-  itemList = [{ title: "Ese1", price: 10, isActive: true }, { title: "Ese2", price: 20, isActive: false }];
-
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
     console.log("jõudsin cart componenti");
+    this.cartItems = this.cartService.cartItemsInService;
   }
 
+  // :{ title: string, price: number, imgSrc: string, category: string; isActive: boolean }
+  onRemoveFromCart(cartItem: any) {
+    let index = this.cartItems.indexOf(cartItem);
+    this.cartItems.splice(index, 1);
+  }
+
+  onEmptyCart() {
+    this.cartItems = [];
+  }
+
+  // ostukorvi kogusumma - forEach abil
+  // js forEach sum object values
 }
+
+
+// onEmptyCart() funktsioon peab olema TS-s
+// jälgige, et oleks klassi loogeliste sulgude sees
+// jälgige, et kogemata ei läheks mõne teise funktsiooni sisse
+// () {}  tavalised sulud ja loogelised sulud
+// loogeliste sulgude sisse vaadake mis JavaScriptis tühjendamiseks
+//      kasutasime ja tehke ka siin
+
+// let on lokaalne muutuja mis on nähtav tema loogeliste sulgude sees
+// this. ehk ülemine klassimuutuja on kasutatav HTMLs ja mitmes funktsioonis
